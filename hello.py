63aa7cce7f7f056580d28c433b21018d662f5780
Rushab Roihan <43216310+rushab101@users.noreply.github.com>
2021-09-22 16:36:52 -0400

    Done Activity 3
    Stolen Time Code from page 34 --> Flask Movement

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
 
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+#curr_time = datetime.now()
+moment = Moment(app)
 
 @app.route("/")
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template("index.html",current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
- return '<h1>Hello, %s!</h1>' % name
+    return render_template("user.html", name=name,  current_time=datetime.utcnow())
 
 if __name__ == "__main__":
     app.run(debug=True)
